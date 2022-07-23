38a34b7b05ada071cbabbb52b74d21ba3c3082e1
Suraj Kumar Sahani <92990594+Surajks025@users.noreply.github.com>
2022-07-24 00:24:59 +0530

    Add files via upload

---
+++ poeval.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+int stk[30],top=-1;
+void push(int x){
+    top++;
+    stk[top]=x;
+    return ;
+}
+int pop(){
+    top--;
+    return stk[top+1];
+}
+int main(){
+    char exp[30];
+    printf("\nEnter the expression : ");
+    gets(exp);
+    int i=0,n=strlen(exp);
+    do{
+        if(exp[i]=='+'){
+            int a=pop(),b=pop();
+            push(a+b);
+        }
+        else if(exp[i]=='-'){
+            int a=pop(),b=pop();
+            push(a-b);
+        }
+        else if(exp[i]=='*'){
+            int a=pop(),b=pop();
+            push(a*b);
+        }
+        else if(exp[i]=='/'){
+            int a=pop(),b=pop();
+            push(a/b);
+        }
+        else{
+            push(exp[i]-'0');
+        }
+        i++;
+    }while(i<n);
+    printf("\n%s = %d\n\n",exp,stk[top]);
+    return 0;
+}
